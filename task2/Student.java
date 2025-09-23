538b5788d83102f3376d422a0e3f8df1b571c665
pillasandeep <155517436+pillasandeep@users.noreply.github.com>
2025-09-23 19:42:26 +0530

    Create Student.java

---
+++ task2/Student.java
@@ -0,0 +1,16 @@
+class Student {
+    int id;
+    String name;
+    double marks;
+
+    public Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Marks: " + marks;
+    }
+}
